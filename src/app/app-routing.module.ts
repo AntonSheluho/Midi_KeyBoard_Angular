b73a88b36763601b4da370b9e1c1537559f9e3ff
Anton_Shalukho <grinfrend2@gmail.com>
2022-06-19 13:04:22 +0200

    add css in app.comp.css, page_not_f.comp, first_page.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageComponent } from './first-page/page/page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'page', component: PageComponent},
+  { path: '',   redirectTo: 'page', pathMatch: 'full' },
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
